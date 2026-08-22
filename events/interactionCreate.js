dc720372eb330a9511511c1c82a67915c0d5baa6
omegarlss1 <omega.rlss1@gmail.com>
2026-08-22 16:53:19 -0300

    Atualizar o interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,16 @@
-
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction, client) {
+    if (!interaction.isChatInputCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+      await command.execute(interaction, client);
+    } catch (e) {
+      console.error(e);
+      if (!interaction.replied) {
+        await interaction.reply({ content: '❌ Erro: ' + e.message, ephemeral: true }).catch(()=>{});
+      }
+    }
+  }
+};
