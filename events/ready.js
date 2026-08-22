e11211b0a15237a408a46adb05fd7dcc409c285a
omegarlss1 <omega.rlss1@gmail.com>
2026-08-22 16:58:17 -0300

    Atualizar o ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,13 @@
-const { Routes, REST } = require('discord.js');
 module.exports = {
   name: 'ready',
   once: true,
   async execute(client) {
     console.log(`Ômot online como ${client.user.tag}!`);
-    const commands = [];
-    for (const cmd of client.commands.values()) commands.push(cmd.data.toJSON());
-    const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-    await rest.put(Routes.applicationCommands(client.user.id), { body: commands });
-    console.log('Comandos /call registrados!');
+    try {
+      for (const [id, guild] of client.guilds.cache) {
+        await guild.commands.set(client.commands.map(c => c.data));
+      }
+      console.log('Comandos /call registrados!');
+    } catch(e) { console.error(e); }
   }
 };
