eea2c288e680b8372b61b1b256d42f5f01aa6a87
omegarlss1 <omega.rlss1@gmail.com>
2026-08-22 16:43:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,24 @@ const fs = require('fs');
 const path = require('path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const express = require('express');
-
 const app = express();
 app.get('/', (req, res) => res.send('Omot online!'));
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Web server rodando na porta ${PORT}`));
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
-});
-
+app.listen(PORT, () => console.log(`Web server na porta ${PORT}`));
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 client.commands = new Collection();
 client.callsTemporarias = new Map();
-
 const commandsPath = path.join(__dirname, 'commands');
 for (const file of fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'))) {
   const cmd = require(path.join(commandsPath, file));
-  client.commands.set(cmd.data.name, cmd);
+  if (cmd.data && cmd.data.name) {
+    client.commands.set(cmd.data.name, cmd);
+  }
 }
-
 const eventsPath = path.join(__dirname, 'events');
 for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'))) {
   const event = require(path.join(eventsPath, file));
   if (event.once) client.once(event.name, (...args) => event.execute(...args, client));
   else client.on(event.name, (...args) => event.execute(...args, client));
 }
-
 client.login(process.env.TOKEN);
