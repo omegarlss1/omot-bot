709c433bb3a61761799e353ec7bb2eb344defd19
omegarlss1 <omega.rlss1@gmail.com>
2026-08-22 23:12:36 -0300

    Criar calls.js

---
+++ utils/calls.js
@@ -0,0 +1,26 @@
+const { salvarCall } = require('./database');
+
+function nomeCallPara(dados, totalMembros) {
+  if (totalMembros <= 1) return `${dados.game} | ${dados.donoNome}`;
+  if (totalMembros === 2) return `${dados.game} | ${dados.donoNome} +1 Ômigo`;
+  return `${dados.game} | ${dados.donoNome} +${totalMembros - 1} Ômigos`;
+}
+
+async function renomearCall(canal, dados) {
+  const nomeFinal = nomeCallPara(dados, canal.members.size);
+  if (canal.name !== nomeFinal) await canal.setName(nomeFinal).catch(() => {});
+}
+
+async function transferirDono(client, canal, dadosAtuais, novoDonoMember) {
+  const dadosAtualizados = { ...dadosAtuais, dono: novoDonoMember.id, donoNome: novoDonoMember.displayName };
+  client.callsTemporarias.set(canal.id, dadosAtualizados);
+  salvarCall(canal.id, dadosAtualizados);
+
+  await canal.permissionOverwrites.delete(dadosAtuais.dono).catch(() => {});
+  await canal.permissionOverwrites.edit(novoDonoMember.id, { ManageChannels: true, MoveMembers: true }).catch(() => {});
+  await renomearCall(canal, dadosAtualizados);
+
+  return dadosAtualizados;
+}
+
+module.exports = { transferirDono, renomearCall, nomeCallPara };
