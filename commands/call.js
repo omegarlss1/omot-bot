61fd576d455672929172543f4d95ba81edc8faf4
omegarlss1 <omega.rlss1@gmail.com>
2026-08-23 17:46:54 -0300

    Update call.js

--- commands/call.js
+++ commands/call.js
@@ -4,14 +4,23 @@ const { salvarGatilho } = require('../utils/database');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('call')
-    .setDescription('Configura as calls temporárias do Ômot')
-    .addChannelOption(o => o.setName('canal').setDescription('Qual canal é o gatilho?').addChannelTypes(ChannelType.GuildVoice).setRequired(true))
+    .setDescription('Define ou remove um canal de voz gatilho')
+    .addChannelOption(o => 
+      o.setName('canal')
+       .setDescription('Selecione o canal de voz gatilho')
+       .addChannelTypes(ChannelType.GuildVoice)
+       .setRequired(true)
+    )
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+
   async execute(interaction, client) {
     const canal = interaction.options.getChannel('canal');
     client.canaisGatilho.add(canal.id);
     await salvarGatilho(canal.id);
 
-    await interaction.reply({ content: `✅ Canal ${canal} salvo no MongoDB como gatilho!`, flags: 64 });
+    await interaction.reply({ 
+      content: `✅ O canal ${canal} foi salvo como **Gatilho de Calls Temporárias**!`, 
+      flags: 64 
+    });
   }
 };
