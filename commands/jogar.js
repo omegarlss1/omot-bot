48094c60bc21de8b168b4c3daf510643e110d32b
omegarlss1 <omega.rlss1@gmail.com>
2026-08-23 20:37:23 -0300

    Atualizar o jogar.js

--- commands/jogar.js
+++ commands/jogar.js
@@ -1,6 +1,10 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { JogoCargo } = require('../utils/jogosDatabase');
 
+// Guardador de tempo de espera por usuário
+const cooldowns = new Map();
+const TEMPO_COOLDOWN = 10 * 60 * 1000; // 10 minutos em milissegundos
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('jogar')
@@ -25,6 +29,22 @@ module.exports = {
         .setRequired(false)),
 
   async execute(interaction, client) {
+    const membroId = interaction.user.id;
+    const agora = Date.now();
+
+    // Verificação de Cooldown
+    if (cooldowns.has(membroId)) {
+      const expiracao = cooldowns.get(membroId) + TEMPO_COOLDOWN;
+
+      if (agora < expiracao) {
+        const tempoRestante = Math.ceil((expiracao - agora) / 1000 / 60);
+        return interaction.reply({
+          content: `❌ Vc precisa esperar **${tempoRestante} min** pra chamar o time de novo.`,
+          flags: 64
+        });
+      }
+    }
+
     await interaction.deferReply();
 
     const jogoKey = interaction.options.getString('jogo');
@@ -51,6 +71,9 @@ module.exports = {
         .setEmoji('🎮')
     );
 
+    // Registra o tempo de uso
+    cooldowns.set(membroId, agora);
+
     return interaction.editReply({
       content: mencaoCargo ? `📢 ${mencaoCargo}` : null,
       embeds: [embed],
@@ -58,5 +81,6 @@ module.exports = {
     });
   }
 };
+
 
 
