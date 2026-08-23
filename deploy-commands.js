4d6d483c78cf459b6c85949bb2e95f167044d8a7
omegarlss1 <omega.rlss1@gmail.com>
2026-08-22 22:07:44 -0300

    Atualizar o deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,15 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('fs');
-const cmds = fs.readdirSync('./commands').filter(f=>f.endsWith('.js')).map(f=>require(`./commands/${f}`).data.toJSON());
+require('dotenv').config();
+
+const cmds = fs.readdirSync('./commands').filter(f => f.endsWith('.js')).map(f => require(`./commands/${f}`).data.toJSON());
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-(async()=>{ await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),{body:cmds}); })();
+
+(async () => {
+  try {
+    await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: cmds });
+    console.log(`${cmds.length} comando(s) registrado(s) com sucesso.`);
+  } catch (e) {
+    console.error(e);
+  }
+})();
