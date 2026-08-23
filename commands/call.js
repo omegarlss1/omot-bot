edc9e007182cf4a4dae2cadad7904ebbbe055c68
omegarlss1 <omega.rlss1@gmail.com>
2026-08-22 22:07:14 -0300

    Atualizar o call.js

--- commands/call.js
+++ commands/call.js
@@ -3,23 +3,22 @@ const { load, save } = require('../utils/database');
 
 module.exports = {
   data: new SlashCommandBuilder()
-   .setName('call')
-   .setDescription('Configura as calls temporárias da Omega')
-   .addChannelOption(o=>o.setName('canal').setDescription('Qual canal é o gatilho?').addChannelTypes(ChannelType.GuildVoice).setRequired(true))
-   .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
-  async execute(interaction, client){
+    .setName('call')
+    .setDescription('Configura as calls temporárias do Ômot')
+    .addChannelOption(o => o.setName('canal').setDescription('Qual canal é o gatilho?').addChannelTypes(ChannelType.GuildVoice).setRequired(true))
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+  async execute(interaction, client) {
     const canal = interaction.options.getChannel('canal');
     client.canaisGatilho.add(canal.id);
 
-    // SALVA NO ARQUIVO
     const db = load();
-    if(!db.gatilhos.includes(canal.id)){
+    if (!db.gatilhos.includes(canal.id)) {
       db.gatilhos.push(canal.id);
       save(db);
     }
 
     const ehJogo = canal.name.toLowerCase().includes('jogo') || canal.name.toLowerCase().includes('divers');
-    const tipo = ehJogo? 'de JOGOS DIVERSOS' : 'PADRÃO';
+    const tipo = ehJogo ? 'de JOGOS DIVERSOS' : 'PADRÃO';
     await interaction.reply({ content: `✅ Canal ${canal} salvo como ${tipo} e **gravado**! Agora mesmo se o bot reiniciar ele vai lembrar.`, ephemeral: true });
   }
 };
