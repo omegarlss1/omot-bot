d3aaa2c191bc3ef83c60cfa7990c53a5d23f6d0d
omegarlss1 <omega.rlss1@gmail.com>
2026-08-25 21:12:13 -0300

    Corrigir caminhos do carregador do bot

--- src/bot/loaders.js
+++ src/bot/loaders.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 function loadCommands(client) {
-  const commandsPath = path.join(__dirname, '../commands');
+  const commandsPath = path.join(__dirname, 'commands');
   const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
 
   for (const file of commandFiles) {
@@ -14,7 +14,7 @@ function loadCommands(client) {
 }
 
 function loadEvents(client) {
-  const eventsPath = path.join(__dirname, '../events');
+  const eventsPath = path.join(__dirname, 'events');
   const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
   for (const file of eventFiles) {
